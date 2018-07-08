1bdf025811e4c079669e855a1db3bd41c2c393d9
isakli05 <40129610+isakli05@users.noreply.github.com>
2018-07-08 13:11:09 +0300

    Initial commit
    ggg

---
+++ 05_05_MVC_Razor/Controllers/RazorHTMLController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _05_05_MVC_Razor.Controllers
+{
+    public class RazorHTMLController : Controller
+    {
+        // GET: RazorHTML
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 05_05_MVC_Razor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_05_05_MVC_Razor.MvcApplication" Language="C#" %>
